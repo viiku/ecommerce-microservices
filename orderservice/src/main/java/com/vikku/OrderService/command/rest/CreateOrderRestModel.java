4f87da4f962f382f680b6599c684e4dcc0bb2a65
viiku <vivekvikkusingh@gmail.com>
2025-02-05 12:40:25 +0530

    Implement Bean validation

--- orderservice/src/main/java/com/vikku/OrderService/command/rest/CreateOrderRestModel.java
+++ orderservice/src/main/java/com/vikku/OrderService/command/rest/CreateOrderRestModel.java
@@ -1,9 +1,19 @@
 package com.vikku.OrderService.command.rest;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+
 public class CreateOrderRestModel {
 
+    @NotBlank(message = "Product ID is a required field")
     private String productId;
+
+    @Min(value = 1, message = "Quantity can not be lower than 1")
+    @Max(value = 5, message = "Quantity can not be greater than 5")
     private int quantity;
+
+    @NotBlank(message = "Address Id is a required field")
     private String addressId;
 
     public String getProductId() {
