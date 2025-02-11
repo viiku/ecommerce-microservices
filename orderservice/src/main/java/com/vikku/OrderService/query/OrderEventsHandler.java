21da519b3b5cfc5daf9474394c746471d2c02f60
viiku <vivekvikkusingh@gmail.com>
2025-02-11 12:48:44 +0530

    Task 9:- Custom error message in JSON format, execution should stop when there is an exception in aggregate class

--- orderservice/src/main/java/com/vikku/OrderService/query/OrderEventsHandler.java
+++ orderservice/src/main/java/com/vikku/OrderService/query/OrderEventsHandler.java
@@ -5,8 +5,10 @@ import com.vikku.OrderService.core.data.OrdersRepository;
 import com.vikku.OrderService.core.events.OrderCreatedEvent;
 import org.axonframework.config.ProcessingGroup;
 import org.axonframework.eventhandling.EventHandler;
+import org.axonframework.messaging.interceptors.ExceptionHandler;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Component;
+
 
 @Component
 @ProcessingGroup("order-group")  // Grouping two event-handler class in to single group
@@ -16,6 +18,18 @@ public class OrderEventsHandler {
 
     OrderEventsHandler(OrdersRepository ordersRepository) {
         this.ordersRepository = ordersRepository;
+    }
+
+    @ExceptionHandler(resultType=Exception.class)
+    public void handle(Exception exception) {
+//        Log error message
+//        Log error message about rolling back transaction
+    }
+
+    @ExceptionHandler(resultType=IllegalArgumentException.class)
+    public void handle(IllegalArgumentException exception) {
+//        Log error message
+//        Log error message about rolling back transaction
     }
 
     @EventHandler
